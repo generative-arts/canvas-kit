6e8fbd0c3ca6a1d91ed7b35402fa240609bb2bb7
nlea <nele.uhlemann@posteo.de>
2021-10-04 15:00:25 +0200

    last changes

--- src/example-github-user.ts
+++ src/example-github-user.ts
@@ -20,6 +20,7 @@ import { JsonExportController } from './controller/utils/jsonexport.controller'
 import { MathController } from './controller/utils/math.controller'
 import { Element } from './enums/Element.enum'
 import { Config } from './types/Config.type'
+import { SSL_OP_SINGLE_ECDH_USE } from 'constants'
 
 async function run() {
   const color = [
@@ -48,42 +49,7 @@ async function run() {
   const middlePointBigCircleY = config.height / 2
   const radiusBigCircle = config.height / 4
   const iterations = 800
-  const slices = 4000
-
-  for (let i = 0; i < slices; i++) {
-    const xCenter = (i * config.width * 2) / slices - config.width / 2
-    const bezierBalanceX = config.width / 10
-    const bezierBalanceY = config.height / 3
-
-    BezierController.bezier(config, {
-      parameters: {
-        start: {
-          x: xCenter,
-          y: config.height,
-        },
-        end: {
-          x: xCenter + 200 + MathController.random(0, 20),
-          y: MathController.random(0, config.height / 5),
-        },
-        bezier1: {
-          x:
-            xCenter - bezierBalanceX - MathController.random(0, bezierBalanceX),
-          y: 2 * bezierBalanceY,
-        },
-        bezier2: {
-          x: xCenter + bezierBalanceX,
-          y: bezierBalanceY,
-        },
-      },
-      color: {
-        stroke: config.colors[MathController.random(0, 3)],
-        //config.colors[Math.round(4*i* (1/(slices/2)))]
-        //config.colors[Math.round(i * (1 / (slices / config.colors.length)))],
-      },
-      closePath: false,
-      element: Element.BEZIER,
-    })
-  }
+  const slices = 2000
 
   const generationConfig: EllipseConfig[] = []
 
@@ -97,7 +63,7 @@ async function run() {
       radiusY: radiusBigCircle,
     },
     color: {
-      fill: config.colors[0],
+      stroke: config.colors[0],
     },
     element: Element.ELLIPSE,
   }
@@ -128,7 +94,41 @@ async function run() {
         radiusY: radiusSmallCircle,
       },
       color: {
-        stroke: config.colors[MathController.random(1, 3)],
+        stroke: config.colors[MathController.random(4, 8)],
+      },
+      element: Element.ELLIPSE,
+    }
+    generationConfig.push(ellipseConfig)
+    EllipseController.ellipse(config, ellipseConfig)
+  }
+
+  for (let i = 0; i < slices; i++) {
+    const winkel = MathController.random(0, 360)
+
+    const Xmax = radiusBigCircle * Math.sin(winkel) + middlePointBigCircleX
+    const Ymax = radiusBigCircle * Math.cos(winkel) + middlePointBigCircleY
+
+    const radiusSmallCircle = MathController.random(0, 240)
+
+    const ellipseConfig: EllipseConfig = {
+      parameters: {
+        coordinate: {
+          x:
+            Xmax +
+            (radiusSmallCircle / radiusBigCircle) *
+              (middlePointBigCircleX - Xmax) *
+              -1,
+          y:
+            Ymax +
+            (radiusSmallCircle / radiusBigCircle) *
+              (middlePointBigCircleX - Ymax) *
+              -1,
+        },
+        radiusX: radiusSmallCircle,
+        radiusY: radiusSmallCircle,
+      },
+      color: {
+        stroke: config.colors[MathController.random(0, 6)],
       },
       element: Element.ELLIPSE,
     }
